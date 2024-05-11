b546af78a9e1bb745e976fc67e817a013f02094a
Hardelle Briones <111712687+HardelleBriones@users.noreply.github.com>
2024-05-11 15:50:02 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 #from . import models
 # from .database import engine
 from routers import query, knowledge_base, user, evaluation, ingest_data
@@ -12,5 +13,7 @@ app.include_router(query.router)
 app.include_router(ingest_data.router)
 app.include_router(knowledge_base.router)
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
 
