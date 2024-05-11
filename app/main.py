e923b1de01f9591aabe2959d4eedf206480ab54e
Hardelle Briones <hardelle.briones@cit.edu>
2024-05-11 16:26:46 +0800

    clean the code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import uvicorn
 #from . import models
 # from .database import engine
 from routers import query, knowledge_base, user, evaluation, ingest_data
@@ -13,7 +12,6 @@ app.include_router(query.router)
 app.include_router(ingest_data.router)
 app.include_router(knowledge_base.router)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
 
 
